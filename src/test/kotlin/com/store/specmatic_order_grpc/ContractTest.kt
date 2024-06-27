b4f93775bf6ce33597e332d4e355107853ba929c
Hari Krishnan <harikrishnan83@gmail.com>
2024-06-27 19:22:34 +0530

    switching to SpringBootTest annotation instead of using ApplicationContext to start and stop the app

--- src/test/kotlin/com/store/specmatic_order_grpc/ContractTest.kt
+++ src/test/kotlin/com/store/specmatic_order_grpc/ContractTest.kt
@@ -3,27 +3,17 @@ package com.store.specmatic_order_grpc
 import `in`.specmatic.grpc.junit.SpecmaticGrpcContractTest
 import `in`.specmatic.grpc.utils.HOST
 import `in`.specmatic.grpc.utils.PORT
-import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
-import org.springframework.boot.SpringApplication
-import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.boot.test.context.SpringBootTest
 
+@SpringBootTest
 class ContractTest : SpecmaticGrpcContractTest {
     companion object {
-        private lateinit var context: ConfigurableApplicationContext
-
         @JvmStatic
         @BeforeAll
         fun setup() {
             System.setProperty(HOST, "localhost")
             System.setProperty(PORT, "9090")
-            context = SpringApplication.run(SpecmaticOrderGrpcApplication::class.java)
-        }
-
-        @JvmStatic
-        @AfterAll
-        fun tearDown() {
-            context.stop()
         }
     }
 }
