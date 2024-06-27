b6aabd51339e1dcf67ea481e0f6b683ebad2446b
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2024-06-27 12:04:23 +0530

    Update ContractTest to run the application programmatically

--- src/test/kotlin/com/store/specmatic_order_grpc/ContractTest.kt
+++ src/test/kotlin/com/store/specmatic_order_grpc/ContractTest.kt
@@ -5,20 +5,25 @@ import `in`.specmatic.grpc.utils.HOST
 import `in`.specmatic.grpc.utils.PORT
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
+import org.springframework.boot.SpringApplication
+import org.springframework.context.ConfigurableApplicationContext
 
 class ContractTest : SpecmaticGrpcContractTest {
     companion object {
+        private lateinit var context: ConfigurableApplicationContext
 
         @JvmStatic
         @BeforeAll
         fun setup() {
             System.setProperty(HOST, "localhost")
             System.setProperty(PORT, "9090")
+            context = SpringApplication.run(SpecmaticOrderGrpcApplication::class.java)
         }
 
         @JvmStatic
         @AfterAll
         fun tearDown() {
+            context.stop()
         }
     }
 }
